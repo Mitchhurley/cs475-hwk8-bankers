31bfc21432222ccd3e527e49b13749761e39ba60
Mitch <mdhurley@pugetsound.edu>
2023-04-27 19:47:07 +0000

    Addeother nat and vector methods

--- vector.c
+++ vector.c
@@ -6,7 +6,7 @@
 
 int** mat_clone(int** mat, int col, int rows){
     //allocate for the clone matrix head
-    int** clone = malloc(rows * sizeof(int));
+    int** clone = malloc(rows * sizeof(int*));
     //using the size params, malloc for the rows
     for (int i = 0; i < rows; i++){
         clone[i] = malloc(col * sizeof(int));
@@ -15,4 +15,42 @@ int** mat_clone(int** mat, int col, int rows){
         }
     }
     return clone
+}
+
+void add_mats(int** matrix1, int** matrix2, int rows, int cols, int** result_matrix) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result_matrix[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+}
+
+void sub_mats(int** matrix1, int** matrix2, int rows, int cols, int** result_matrix) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result_matrix[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+}
+
+int* vec_clone(int* vec, int length){
+    //allocate for the clone vector
+    int* clone = malloc(length * sizeof(int));
+    //using the size params, copy the data
+    for (int i = 0; i < length; i++){
+        clone[i] = vec[i];
+    }
+    return clone
+}
+
+void add_vecs(int* vec1, int* vec2, int size, int* result_vec) {
+    for (int i = 0; i < size; i++) {
+        result_vec[i] = vec1[i] + vec2[i];
+    }
+}
+
+void sub_vecs(int* vec1, int* vec2, int size, int* result_vec) {
+    for (int i = 0; i < size; i++) {
+        result_vec[i] = vec1[i] - vec2[i];
+    }
 }
--- vector.h
+++ vector.h
@@ -1,3 +1,8 @@
 
 // TODO: function prototypes of vector and matrix operations
-int * mat_clone(int** mat, int col, int rows);
+int ** mat_clone(int** mat, int col, int rows);
+void add_mats(int** matrix1, int** matrix2, int rows, int cols, int** result_matrix);
+void sub_mats(int** matrix1, int** matrix2, int rows, int cols, int** result_matrix);
+int* vec_clone(int* vec, int length);
+void add_vecs(int* vec1, int* vec2, int size, int* result_vec);
+void sub_vecs(int* vec1, int* vec2, int size, int* result_vec);
