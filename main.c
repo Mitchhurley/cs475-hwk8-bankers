aaef8f169328bebe386d67cfc4bfdffba93e6441
Jared Vigliecca <jaredvig@gmail.com>
2023-04-27 23:24:37 +0000

    pre-processed the data and created the matrices and vector needed, as well as did sanity checks and freed all memory used by these matrices.

--- main.c
+++ main.c
@@ -3,6 +3,24 @@
 #include "vector.h"
 #include "banker.h"
 
+void endAndFree(int error, int numResources, int numProcesses, int**maxDemandMatrix,int** allocationMatrix, int*totalResourceVector){
+  for(int i = 0; i <numProcesses;i++){
+    free(maxDemandMatrix[i]);
+    maxDemandMatrix[i] = NULL;
+    free(allocationMatrix[i]);
+    allocationMatrix[i]=NULL;
+  }
+  free(maxDemandMatrix);
+  maxDemandMatrix = NULL;
+  free(allocationMatrix);
+  allocationMatrix = NULL;
+  free(totalResourceVector);
+  totalResourceVector = NULL;
+  if(error==1){
+    exit(1);
+  }
+  return;
+}
 int main(int argc, char *argv[])
 {
   // TODO: attempt to open scenario file and scan data into allocated structures
@@ -18,8 +36,49 @@ int main(int argc, char *argv[])
   }
   int numResources;
   int numProcesses;
+  fscanf(pFile,"%d",&numResources);
+  fscanf(pFile,"%d",&numProcesses);
   
+  int* totalResourceVector = (int*)malloc(sizeof(int)*numResources);
+  int** maxDemandMatrix = (int**)malloc(sizeof(int*)*numProcesses);
+  int** allocationMatrix = (int**)malloc(sizeof(int*)*numProcesses);
+  int error = 0;
+  for(int i = 0;i<numResources;i++){
+    int totalResource;
+    fscanf(pFile,"%d\n",&totalResource);
+    totalResourceVector[i] =totalResource;
+  }
+  for(int i = 0;i<numProcesses;i++){
+    maxDemandMatrix[i] = (int*)malloc(sizeof(int)*numResources);
+        allocationMatrix[i] = (int*)malloc(sizeof(int)*numResources);
+    for(int j = 0;j<numResources;j++){
+      int maxDemand;
+      fscanf(pFile,"%d\n",&maxDemand);
+      maxDemandMatrix[i][j] = maxDemand;
+    }
+  }
+  for(int i = 0;i<numProcesses;i++){
+    for(int j = 0;j<numResources;j++){
+      int allocation;
+      fscanf(pFile,"%d\n",&allocation);
+      if(error==0 && allocation>totalResourceVector[j]){
+        printf("Integrity test failed: allocated resources exceed total resources\n");
+        error = 1;
+      }
+      if(error==0 && allocation>maxDemandMatrix[i][j]){
+        int diff = maxDemandMatrix[i][j]-allocation;
+        printf("Integrity test failed: allocated resources exceed demand for thread %d\n",i);
+        printf("Need %d instances of resource %d",diff,j);
+        error = 1;
+      }
+      allocationMatrix[i][j] = allocation;
+    }
+  }
+  fclose(pFile);
+  if(error==1){
+    endAndFree(error,numResources,numProcesses,maxDemandMatrix,allocationMatrix,totalResourceVector);
+  }
   // TODO: Run banker's safety algorithm
-
+  endAndFree(0,numResources,numProcesses,maxDemandMatrix,allocationMatrix,totalResourceVector);
   return 0;
 }
