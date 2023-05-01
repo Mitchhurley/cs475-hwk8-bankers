33126e06ed51595783d641ad1336c8252f44a59d
Mitch <mdhurley@pugetsound.edu>
2023-05-01 19:03:28 +0000

    added recursive funtion, now just need to call it

--- banker.c
+++ banker.c
@@ -11,39 +11,49 @@ void isSafe(int* available, int** alloc, int** need, int m, int n){
     int* work = vec_clone(available, m);
     //init finish array
     int* finish = malloc(sizeof(int) * n);
-    for(int i =0;i < n;i++){
-        finish[i] = 0;
-    }
     //keep track of safe sequence
-    int** safe_seq = malloc(sizeof(int*) * n);
+    int** safe_seq = malloc(sizeof(int*) * (n * n));
     for (int i = 0; i < n; i++) {
         safe_seq[i] = malloc(sizeof(int) * n);
     }
+    int seq_top = -1;
     //keep track of number of safe sequences
     int count = 0;
     //Find thread that can be finished with available reasources
     int i = 0;
-    while(count<n) {
+    int seq_pos = 0;
+    //do this until all are found
+    while(count<(n * n)) {
+        print("Current Finish: " + print_vec(finish, n) + "\n");
         for (i = 0; i < n; i++) {
+            //loops through to find unfinished thread
             if (finish[i] == 0) {
+                //now we found the thread, lets loop through its needs to make sure they are met
                 int canExecute = 1;
                 int j = 0;
+                print("Thread "+ i + " is unfinished, testing to see if reasources can be allocated\n");
                 for (j = 0; j < m; j++) {
+                    //for each need of the thread, check if we have the reasources
                     if (need[i][j] > work[j]) {
+                        //if it cant be met, set cant execute to false
                         canExecute = 0;
+                        print("Thread "+ i + " cant finish because reasource " + j " is needed\n");
                     }
                 }
-                if (canExecute) {
+                if (canExecute) { 
+                    print("Thread "+ i + " can finish, freeing reasources\n");
                     int k = 0;
+                    print("prev work" + print_vec(work[k], m)+ "\n");
                     for (k = 0; k < m; k++) {
+                        //simulate freeing of reasources
                         work[k] += alloc[i][k];
                     }
+                    //set the finish value for the thread to true
+                    print("curr work work" + print_vec(work[k], m)+ "\n");
                     finish[i] = 1;
                     // Add thread ID to current safe sequence
-                    for (k = 0; k < n; k++) {
-                        safe_seq[count][k] = (k < count ? safe_seq[k][k] : -1);
-                    }
-                    safe_seq[count][count] = i;
+                    safe_seq[count][seq_pos++] = i;
+                    
                     if (count == 0 || safe_seq[count][n-1] != -1) {
                         // If current safe sequence is complete, increment number of schedules found
                         count++;
@@ -122,4 +132,42 @@ int ** subtractMatrices(int** matrix1, int** matrix2, int outerLength, int inner
         }
     }
     return diffMatrix;
+}
+
+bool recursive_test(int* available, int** alloc, int** need, int m, int n, int depth, int* finished,  int* cur_seq){
+    //Create the work vector from available
+    int* work = vec_clone(available, m);
+    //init finish array
+    int* finish = vec_clone(finished, n);;
+    //keep track of safe sequence
+    int* safe_seq = vec_clone(cur_seq, n)
+    for (i = 0; i < n; i++) {
+            //loops through to find unfinished thread
+            if (finish[i] == 0) {
+                int j = 0;
+                for (j = 0; j < m; j++) {
+                    //for each need of the thread, check if we have the reasources
+                    if (need[i][j] > work[j]) {
+                        //if it cant be met, set cant execute to false
+                        free(finish);
+                        free(safe_seq);
+                        free(work);
+                        return false;
+                    }
+                }
+                for (k = 0; k < m; k++) {
+                    //simulate freeing of reasources
+                    work[k] += alloc[i][k];
+                }
+                //set the finish value for the thread to true
+                finish[i] = 1;
+                safe_seq[depth] = i;
+                if (recursive_test(work, alloc, need, m, n, depth + 1, finish, safe_seq)){
+                    free(finish);
+                    free(safe_seq);
+                    free(work);
+                    return true;
+                }
+            }
+    }
 }
--- banker.h
+++ banker.h
@@ -2,3 +2,4 @@
 void isSafe(int* available, int** alloc, int** need, int m, int n);
 void print_schedules(int* finish, int** sequence, int num_schedules, int n); 
 int ** subtractMatrices(int** matrix1, int** matrix2, int outerLength, int innerLength);
+bool recursive_test(int* available, int** alloc, int** need, int m, int n, int depth, int* finished,  int* cur_seq);
